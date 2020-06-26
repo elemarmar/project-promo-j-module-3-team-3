8ff9bc9493ba201e0509cbfda62f7fcdec2f712e
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-06-26 12:37:54 +0200

    Add collapsable js example for Design

--- src/js/01-contslet.js
+++ src/js/01-contslet.js
@@ -1,1 +1,23 @@
-"use strict";
+'use strict';
+
+//COLAPSABLE
+const design = document.querySelector('.container__design--title');
+const designCollapsable = document.querySelector('.palettes__container');
+
+
+
+function showCollapsable() {
+  console.log("Hello");
+  if (designCollapsable.classList.contains('hidden')) {
+    console.log("remove hidden");
+    designCollapsable.classList.remove('hidden'); // quita hidden
+
+    // otros: añadir hidden
+  } else {
+    designCollapsable.classList.add('hidden');
+    console.log("add hidden");
+    // pon hidden
+  }
+
+}
+design.addEventListener('click',showCollapsable);
