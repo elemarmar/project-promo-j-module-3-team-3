8e77d90f7c9122ce4f398092132e3e0bf606c5fa
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-07-01 20:50:01 +0200

    Add flexible card background

--- src/js/02-contslet.js
+++ src/js/02-contslet.js
@@ -19,6 +19,7 @@ function showCollapsible(event) {
     colArrow[0].classList.toggle('arrow-active');
     colArrow[1].classList.remove('arrow-active');
     colArrow[2].classList.remove('arrow-active');
+    card.classList.remove('large');
 
   // Caso 2: Pulsamos fill
   } else if (event.target === colTitle[1] || event.target === colArrow[1]) {
@@ -30,6 +31,9 @@ function showCollapsible(event) {
     colArrow[1].classList.toggle('arrow-active');
       colArrow[2].classList.remove('arrow-active');
       
+      // animate card-prewview
+      card.classList.toggle('large');
+      
   // Caso 3: pulsamos share
   } else if (event.target === colTitle[2] || event.target === colArrow[2]) {
     colBox[0].classList.add('hidden');
@@ -38,7 +42,8 @@ function showCollapsible(event) {
       
     colArrow[0].classList.remove('arrow-active');
     colArrow[1].classList.remove('arrow-active');
-    colArrow[2].classList.toggle('arrow-active');
+      colArrow[2].classList.toggle('arrow-active');
+      card.classList.remove('large');
   }
 }
 
