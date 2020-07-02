b2e4b97cd48c0fe38a118ed602775733452fc287
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-07-02 12:22:36 +0200

    Merge master to dev

--- src/js/02-contslet.js
+++ src/js/02-contslet.js
@@ -9,7 +9,8 @@ function showCollapsible(event) {
   const colBox = document.querySelectorAll(".js-collapsibleChild");
 
   // Caso 1: Pulsamos design
-<<<<<<< HEAD:src/js/01-contslet.js
+    card.classList.remove("large");
+    
   if (
     event.target === colTitle[0] ||
     event.target === colArrow[0] ||
@@ -46,48 +47,13 @@ function showCollapsible(event) {
     event.target === colArrow[2] ||
     event.target === colDiv[2]
   ) {
-    colBox[0].classList.add("hide-desing");
+    colBox[0].classList.add("hide-design");
     colBox[1].classList.add("hide-fill");
     colBox[2].classList.toggle("hide-share");
 
     colArrow[0].classList.remove("arrow-active");
     colArrow[1].classList.remove("arrow-active");
     colArrow[2].classList.toggle("arrow-active");
-=======
-  if (event.target === colTitle[0] || event.target === colArrow[0]) {
-    colBox[0].classList.toggle('animate-design');
-    colBox[1].classList.add('animate-fill');
-    colBox[2].classList.add('animate-share');
-
-    colArrow[0].classList.toggle('arrow-active');
-    colArrow[1].classList.remove('arrow-active');
-    colArrow[2].classList.remove('arrow-active');
-    card.classList.remove('large');
-
-  // Caso 2: Pulsamos fill
-  } else if (event.target === colTitle[1] || event.target === colArrow[1]) {
-    colBox[0].classList.add('animate-design');
-    colBox[1].classList.toggle('animate-fill');
-    colBox[2].classList.add('animate-share');
-      
-    colArrow[0].classList.remove('arrow-active');
-    colArrow[1].classList.toggle('arrow-active');
-      colArrow[2].classList.remove('arrow-active');
-      
-      // animate card-prewview
-      card.classList.toggle('large');
-      
-  // Caso 3: pulsamos share
-  } else if (event.target === colTitle[2] || event.target === colArrow[2]) {
-    colBox[0].classList.add('animate-design');
-    colBox[1].classList.add('animate-fill');
-    colBox[2].classList.toggle('animate-share');
-      
-    colArrow[0].classList.remove('arrow-active');
-    colArrow[1].classList.remove('arrow-active');
-      colArrow[2].classList.toggle('arrow-active');
-      card.classList.remove('large');
->>>>>>> tmp:src/js/02-contslet.js
   }
 }
 
