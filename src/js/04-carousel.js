dc335a0c8c08783784ad848f873e3cde6364754b
Ana Fernandez <anafdz92@gmail.com>
2020-07-04 10:26:08 +0200

    temporal breakdown

---
+++ src/js/04-carousel.js
@@ -0,0 +1,22 @@
+// Slide posición 1 se muestra (demás -> hidden)
+// ---> next -->> mostrar slide posición (posición actual + 1)
+// ---> prev -->> mostrar slide posición (posición actual - 1)
+
+// Función que muestra slide elegido y esconde las otras
+// function(posicion actual + 1)
+
+// Estamos en Bernard: posición 1
+// ---> pulsamos next
+// console.log("H");
+// function showSlide() {
+//   let slides = document.querySelectorAll(".js-slide");
+
+//   // esconder todos
+//   for (let = i; i < slides.length; i++) {
+//     slides[i].classList.add("hidden");
+//   }
+
+//   // mostrar elegido
+// }
+
+// showSlide();
