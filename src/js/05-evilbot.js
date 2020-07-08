e0f32f03f65c481028e6e704fd8fba3e8fb0a0ef
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-07-08 14:09:47 +0200

    Add random number for all arrays

--- src/js/05-evilbot.js
+++ src/js/05-evilbot.js
@@ -1,3 +1,5 @@
+let evilChat = document.querySelector(".evil-chat");
+
 //Frases
 const greetings = [
   `Diría que me alegro de verte, pero mentiría`,
@@ -11,20 +13,14 @@ function randomN(max) {
    return Math.floor(Math.random() * max) 
 }
 
-let evilChat = document.querySelector(".evil-chat");
-
-// random number: 0
-
 evilChat.innerHTML= greetings[randomN(greetings.length)];
-
-
 
 
 const name = [
   `{name}, ¿sabes que he venido a conquistar tu mundo?`,
   `Encantado, {name}. Me alegra conocer tu nombre antes de acabar contigo`,
 ];
-
+// evilChat.innerHTML= name[randomN(name.length)];
 
 const palettesReaction = [
   `¡Qué color más absurdo!`,
@@ -32,50 +28,63 @@ const palettesReaction = [
   `¿Esos colores? ¿En serio?`,
   `¡Ese es perfecto! Buena elección.`,
 ];
+// evilChat.innerHTML= palettesReaction[randomN(palettesReaction.length)];
 
 const imageReaction = [
   `Tienes una belleza muy particular`,
   `Bueno, si no tienes otra mejor...`,
 ];
+// evilChat.innerHTML= imageReaction[randomN(imageReaction.length)];
+
 
 const jobReaction = [
   `Vaya, como {job} debes tener buen sueldo ¡Qué pena que vaya a destruir el mundo`,
   `¿Trabajar? Por eso los humanos sois una especie inferior`,
 ];
+// evilChat.innerHTML= jobReaction[randomN(jobReaction.length)];
 
 const emailReaction = [
   `Pero, ¡revisa luego los correos que te lleguen!`,
   `¿Email? ¿Todavía no domináis la telepatía?`,
 ];
+// evilChat.innerHTML= emailReaction[randomN(emailReaction.length)];
 
 const phoneReaction = [
   `Da igual que pongas tu teléfono porque siempre tienes el móvil en silencio`,
   `El teléfono no es obligatorio, porque a ti nunca te llama nadie`,
 ];
+// evilChat.innerHTML= phoneReaction[randomN(phoneReaction.length)];
 
 const linkedinReaction = [
   `Linkedin es el instagram de los trabajos. ¡Todo es potureo!`,
   `Actualiza tu foto de Linkedin que la que tienes es de hace 10 años`,
 ];
+// evilChat.innerHTML= linkedinReaction[randomN(linkedinReaction.length)];
+
 const githubReaction = [
   `¡Gracias! Así podré robarte tu código`,
   `¿Sabes que tener github no te convierte en programadora, verdad?`,
 ];
+// evilChat.innerHTML= githubReaction[randomN(githubReaction.length)];
 
 const createCard = [
   `Ya tienes lo que querías, ¿me puedes dejar en paz?`,
   `Estupendo, ahora tus datos están en mi poder ¡muahaha!`,
 ];
+// evilChat.innerHTML= createCard[randomN(createCard.length)];
+
 const reset = [
   `¡No estás conforme con nada! ¿te apetece destruir el mundo conmigo?`,
   `Equivocarse es humano...y los humanos deben ser destruidos`,
 ];
+// evilChat.innerHTML= reset[randomN(reset.length)];
 
 const hooverBoot = [
   `¡ Quita tus sucias manos de encima, humano!`,
   `¿Se puede saber que demonios haces?`,
   `¡Acabaré contigo y con tu ridículo planeta!`,
 ];
+// evilChat.innerHTML= hooverBoot[randomN(hooverBoot.length)];
 
 let missingInputs = [];
 // Te falta rellenar: "bla, bla, bla, bla"
